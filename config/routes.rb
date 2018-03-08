50ac76000020a67636f6aa3761fbcf1471e40bd2
kryswebdev <krystlewebdev@gmail.com>
2018-03-08 22:02:54 +0000

    3.8 i made 3 pages, and added some basic content

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'simple_pages/index'
+
+    get 'simple_pages/contact'
+
+    get 'simple_pages/about' 
+
+    get 'simple_pages/index' #These are for the index page
+    root 'simple_pages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#All pages in your app need have corresponding controllers in order to be displayed.That controller needs to be referenced inside here, the routes files.
