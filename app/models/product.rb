8a1d5bc5232382045c0a5cadd3d927ebf4b107b9
Kryswebdev <krystle.webdev@gmail.com>
2018-03-29 23:32:29 +0100

    add two new file & modified app.css

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
 end
+
+# this code is tied to a database table called products, and that table will store the data for all the products in our shop. We'll do the same with users, orders, etc.
