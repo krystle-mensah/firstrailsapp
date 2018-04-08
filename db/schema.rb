dab7f66f0967a6f0be5c680a261c852ae1d1dda3
Kryswebdev <krystle.webdev@gmail.com>
2018-04-08 21:30:34 +0100

    added new images and scss page

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330173044) do
+ActiveRecord::Schema.define(version: 20180404171908) do
 
-    create_table "products", force: :cascade do |t|
-        t.string "name"
-        t.text "description"
-        t.string "image_url"
-        t.datetime "created_at", null: false
-        t.datetime "updated_at", null: false
-        t.string "color"
-    end
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "color"
+  end
 
 end
