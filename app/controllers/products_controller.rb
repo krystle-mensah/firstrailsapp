b44b9924dd6919de791213814ae8e224c8141998
Kryswebdev <krystle.webdev@gmail.com>
2018-04-12 11:24:06 +0100

    4.2 - updating variables

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
 	# POST /products
 	# POST /products.json
 	def create
-		@product = Product.new(product_params)
+		@product = Product.new(product_params) 
 
 		respond_to do |format|
 			if @product.save
@@ -72,6 +72,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-		params.require(:product).permit(:name, :description, :image_url, :colour)
+		params.require(:product).permit(:name, :description, :image_url, :color)
     end
 end 
