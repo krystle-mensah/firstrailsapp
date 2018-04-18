c309982d54260ac94ff02f4162bdaf3a90e4fc25
Kryswebdev <krystle.webdev@gmail.com>
2018-04-19 00:25:37 +0100

    modified seeds.rb to test seeding heroku data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 
 # Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
 
-Product.create(name: "console-generated product", description: "I created this product without using the HTML form!", image_url: "http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!" ) 
+
+
