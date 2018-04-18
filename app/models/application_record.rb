43fac8331268bfb64a01a7994965575403009866
Kryswebdev <krystle.webdev@gmail.com>
2018-04-18 21:17:41 +0100

    4.4 - added migration for orders and new order.rb model.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
 end
+
+
+# ActiveRecord::Base is provided by Rails and contains all of the functionality we need to save our models to the database, query our database for our application's data, define relationships between models in our application, and define validations for our models.
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+    has_many :orders
 end
 
 # this code is tied to a database table called products, and that table will store the data for all the products in our shop. We'll do the same with users, orders, etc.
+
+# This declaration means that your product class is a subclass of, or inherits from, the ApplicationRecord class.
