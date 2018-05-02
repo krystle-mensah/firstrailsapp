19e6e03878e46d5208cf1db3c3d827ab1f78c847
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-02 21:18:08 +0100

    modified product.rb+landingpage+

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,10 @@ class Product < ApplicationRecord
     has_many :orders
 end
 
+# represents the product table in the database.
+
+
+
 # this code is tied to a database table called products, and that table will store the data for all the products in our shop. We'll do the same with users, orders, etc.
 
 # This declaration means that your product class is a subclass of, or inherits from, the ApplicationRecord class.
