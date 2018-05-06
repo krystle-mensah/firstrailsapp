a10096de259eadc415bc2832ae36d84f3680d040
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-06 23:02:35 +0100

    redirected root page to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :products
+  
+  get 'simple_pages/landing_page'
+  
   get 'simple_pages/index'
 
-  get 'simple_pages/contact'
+  root 'simple_pages#contact'
 
   get 'simple_pages/about' 
 
   get 'simple_pages/index' 
-  root 'simple_pages#landing_page'
+  
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
