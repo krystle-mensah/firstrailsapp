e05d0338ff140a1ee94be6c792ea8cd68af1ac60
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-07 11:58:56 +0100

    5.1 root path added to nav brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :products
   
+  get '/products', to: 'products#index'
+  
   get 'simple_pages/landing_page'
   
-  get 'simple_pages/index'
+  root 'simple_pages#index'
 
-  root 'simple_pages#contact'
+  get 'simple_pages/contact'
 
   get 'simple_pages/about' 
 
@@ -16,5 +18,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end 
-
+ 
 #All pages in your app need have corresponding controllers in order to be displayed.That controller needs to be referenced inside here, the routes files.
